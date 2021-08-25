3291cfca6aee922135a0943ac79c5c5f18893879
Bobby Missirian <bobbymissirian@Bobbys-MacBook-Pro.local>
2021-08-24 17:12:30 -0700

    Updates

---
+++ precache-manifest.f6165b53c889854cc2aab6ea990d34f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc0893085c8c3ee3fe7a67fb9b7fd29",
+    "url": "/Kahzap/index.html"
+  },
+  {
+    "revision": "2d6877e914951ae42e96",
+    "url": "/Kahzap/static/css/main.ca019e9d.chunk.css"
+  },
+  {
+    "revision": "fc98d61d3ca1540b424b",
+    "url": "/Kahzap/static/js/2.855eda04.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Kahzap/static/js/2.855eda04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d6877e914951ae42e96",
+    "url": "/Kahzap/static/js/main.b9bfa104.chunk.js"
+  },
+  {
+    "revision": "79abd3d7c840c6dc63dd",
+    "url": "/Kahzap/static/js/runtime-main.56e15af5.js"
+  }
+]);
