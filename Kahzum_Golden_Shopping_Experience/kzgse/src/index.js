cc7e9de57b45c0a8e2f55dfe2982b6a578721b25
Bobby Missirian <bobbymissirian@Bobbys-MacBook-Pro.local>
2021-08-24 19:28:16 -0700

    routing

--- Kahzum_Golden_Shopping_Experience/kzgse/src/index.js
+++ Kahzum_Golden_Shopping_Experience/kzgse/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import { HashRouter } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
+    <HashRouter>
     <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
